ff733730d18a0114bed3e034874dc5c1f065590a
Anders Fisher <atleastimtrying@gmail.com>
2012-04-28 10:33:23 +0100

    public updated

--- public/js/application.coffee
+++ public/js/application.coffee
@@ -1,48 +1,23 @@
-class Sight
+class window.App
+  constructor: ->
+    @canvas = $ 'canvas'
+    @ctx = canvas[0].getContext '2d'
+    @resize()
+    $(window).resize @resize
 
-	constructor: (@p5) ->
-		@x = @p5.width/2
-		@y = @p5.height/2
-	draw: ()->
-		@p5.noFill()
-		@p5.stroke(0)
-		@p5.strokeWeight(1200)
-		@p5.ellipse @p5.mouseX,@p5.mouseY, 1500, 1500
-		@p5.noStroke()
-		@p5.fill(0)
-		@p5.rectMode(@p5.CENTER)
-		@p5.rect(@p5.mouseX,@p5.mouseY, 100, 2)
-		@p5.rect(@p5.mouseX,@p5.mouseY, 2, 100)
-		@p5.rectMode(@p5.CORNER)
+  draw: ->
+    @ctx.fillStyle = '#efefef'
+    @ctx.fillRect 0, 0, @width, @height
 
-class Target
-	constructor: (@p5) ->
-		@x = p5.random(p5.width)
-		@y = p5.random(p5.height)
-		@C = @p5.color(@p5.random(255))
+  animate: =>
+    @draw()
+    window.requestAnimationFrame @animate
 
-	draw: ->
-		if @p5.mousePressed && @hitTest()
-			alert "shot"
-		else
-			@p5.fill @C
-			@p5.ellipse @x, @y, 20, 20
-	hitTest: ->
-		@p5.dist(@p5.mouseX, @p5.mouseY, @x,@y) < 10
-sketch = (p5) ->
-	p5.setup = () ->
-		p5.size(960, 400)
-		p5.background(255)
-		p5.smooth()
-		@sight = new Sight(p5)
-		@particles = []
-		@particles.push new Target(p5) for whatever in [1..65]
-
-	p5.draw = () ->
-		p5.background 255
-		p5.noStroke()
-		particle.draw() for particle in @particles
-		@sight.draw()
+  resize: ->  
+    @width = $(window).width
+    @height = $(window).height
+    @canvas.width = @width
+    @canvas.height = @height
 	
 $ ->
-	processing = new Processing ($ "canvas")[0], sketch
+  window.app = new App
--- public/js/application.js
+++ public/js/application.js
@@ -1,73 +1,29 @@
 (function() {
-  var Sight, Target, sketch;
-  Sight = (function() {
-    function Sight(p5) {
-      this.p5 = p5;
-      this.x = this.p5.width / 2;
-      this.y = this.p5.height / 2;
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+  window.App = (function() {
+    function App() {
+      this.animate = __bind(this.animate, this);      this.canvas = $('canvas');
+      this.ctx = canvas[0].getContext('2d');
+      this.resize();
+      $(window).resize(this.resize);
     }
-    Sight.prototype.draw = function() {
-      this.p5.noFill();
-      this.p5.stroke(0);
-      this.p5.strokeWeight(1200);
-      this.p5.ellipse(this.p5.mouseX, this.p5.mouseY, 1500, 1500);
-      this.p5.noStroke();
-      this.p5.fill(0);
-      this.p5.rectMode(this.p5.CENTER);
-      this.p5.rect(this.p5.mouseX, this.p5.mouseY, 100, 2);
-      this.p5.rect(this.p5.mouseX, this.p5.mouseY, 2, 100);
-      return this.p5.rectMode(this.p5.CORNER);
+    App.prototype.draw = function() {
+      this.ctx.fillStyle = '#efefef';
+      return this.ctx.fillRect(0, 0, this.width, this.height);
     };
-    return Sight;
+    App.prototype.animate = function() {
+      this.draw();
+      return window.requestAnimationFrame(this.animate);
+    };
+    App.prototype.resize = function() {
+      this.width = $(window).width;
+      this.height = $(window).height;
+      this.canvas.width = this.width;
+      return this.canvas.height = this.height;
+    };
+    return App;
   })();
-  Target = (function() {
-    function Target(p5) {
-      this.p5 = p5;
-      this.x = p5.random(p5.width);
-      this.y = p5.random(p5.height);
-      this.C = this.p5.color(this.p5.random(255));
-    }
-    Target.prototype.draw = function() {
-      if (this.p5.mousePressed && this.hitTest()) {
-        return alert("shot");
-      } else {
-        this.p5.fill(this.C);
-        return this.p5.ellipse(this.x, this.y, 20, 20);
-      }
-    };
-    Target.prototype.hitTest = function() {
-      return this.p5.dist(this.p5.mouseX, this.p5.mouseY, this.x, this.y) < 10;
-    };
-    return Target;
-  })();
-  sketch = function(p5) {
-    p5.setup = function() {
-      var whatever, _results;
-      p5.size(960, 400);
-      p5.background(255);
-      p5.smooth();
-      this.sight = new Sight(p5);
-      this.particles = [];
-      _results = [];
-      for (whatever = 1; whatever <= 65; whatever++) {
-        _results.push(this.particles.push(new Target(p5)));
-      }
-      return _results;
-    };
-    return p5.draw = function() {
-      var particle, _i, _len, _ref;
-      p5.background(255);
-      p5.noStroke();
-      _ref = this.particles;
-      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-        particle = _ref[_i];
-        particle.draw();
-      }
-      return this.sight.draw();
-    };
-  };
   $(function() {
-    var processing;
-    return processing = new Processing(($("canvas"))[0], sketch);
+    return window.app = new App;
   });
 }).call(this);
