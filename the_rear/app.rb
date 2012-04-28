f10afbca700c588f2f65b6f7d6ca94847951f73e
Dan Higham <dan.higham@gmail.com>
2012-04-28 10:37:09 +0100

    Changed index path

--- the_rear/app.rb
+++ the_rear/app.rb
@@ -1,7 +1,9 @@
 
 
 get '/' do
-  File.read(File.join('public', 'index.html'))
+  file_path = File.join(File.dirname(__FILE__), '..', 'public', "index.html") 
+	content_type File.extname(file_path)
+	File.read(file_path)
 end
 
 get %r{/public/(.+)} do |c|
